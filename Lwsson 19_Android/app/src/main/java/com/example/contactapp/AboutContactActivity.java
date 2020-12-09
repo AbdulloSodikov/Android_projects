1c50d07fa8b47e1b33489d9c88da77df106dfb7c
AbdulloSodikov <sodikov.ak@gmail.com>
2020-12-09 18:40:57 +0500

    update

--- Lwsson 19_Android/app/src/main/java/com/example/contactapp/AboutContactActivity.java
+++
@@ -1,4 +0,0 @@
-package com.example.contactapp;
-
-public class AboutContactActivity {
-}
--- myFirstApp2/app/src/main/java/com/example/myfirstapp2/MainActivity.java
+++ myFirstApp2/app/src/main/java/com/example/myfirstapp2/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.myfirstapp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView newImage;
-
+    TextView textMyApp;
+    ImageView imageMyApp;
+    Button firstButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         newImage = findViewById(R.id.imageMisha);
         newImage = findViewById(R.id.imageMasha);
+        imageMyApp = findViewById(R.id.imageMisha);
+        imageMyApp = findViewById(R.id.imageMasha);
+        textMyApp = findViewById(R.id.title);
+        textMyApp = findViewById(R.id.testHistory);
+
+        firstButton = findViewById(R.id.button);
+        firstButton.setOnContextClickListener();
+
 
     }
 }
